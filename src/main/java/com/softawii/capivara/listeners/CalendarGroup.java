f1f4708d0de2612dcb1fdb93b8fec3a76ad6cb1e
FerroEduardo <47820549+FerroEduardo@users.noreply.github.com>
2023-10-30 11:48:04 -0300

    feat: fix calendar commands

--- src/main/java/com/softawii/capivara/listeners/CalendarGroup.java
+++ src/main/java/com/softawii/capivara/listeners/CalendarGroup.java
@@ -4,43 +4,37 @@ import com.softawii.curupira.annotations.IArgument;
 import com.softawii.curupira.annotations.ICommand;
 import com.softawii.curupira.annotations.IGroup;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@IGroup(name = "Calendar", description = "Calendar events", hidden = false)
+@IGroup(name = "calendar", description = "Calendar events", hidden = false)
 public class CalendarGroup {
 
-    @ICommand(name = "Subscribe", description = "Subscribe to a Google Calendar", permissions = {Permission.ADMINISTRATOR})
-    @IArgument(name = "Calendar ID", description = "The ID of the calendar to subscribe to", required = true)
-    @IArgument(name = "Name", description = "The name to use for the calendar", required = true)
-    @IArgument(name = "Channel", description = "The channel where events will be announced", required = true, type = OptionType.CHANNEL)
-    @IArgument(name = "Role", description = "The role to be pinged when events begin", required = false, type = OptionType.ROLE)
+    @ICommand(name = "subscribe", description = "Subscribe to a Google Calendar", permissions = {Permission.ADMINISTRATOR})
+    @IArgument(name = "calendar-id", description = "The ID of the calendar to subscribe to", required = true)
+    @IArgument(name = "name", description = "The name to use for the calendar", required = true)
+    @IArgument(name = "channel", description = "The channel where events will be announced", required = true, type = OptionType.CHANNEL)
+    @IArgument(name = "role", description = "The role to be pinged when events begin", required = false, type = OptionType.ROLE)
     public static void subscribe(SlashCommandInteractionEvent event) {
         event.reply("Not implemented yet").queue();
     }
 
-    @ICommand(name = "Unsubscribe", description = "Unsubscribe from a Google Calendar", permissions = {Permission.ADMINISTRATOR})
-    @IArgument(name = "Name", description = "The name to use for the calendar", required = true)
+    @ICommand(name = "unsubscribe", description = "Unsubscribe from a Google Calendar", permissions = {Permission.ADMINISTRATOR})
+    @IArgument(name = "name", description = "The name to use for the calendar", required = true)
     public static void unsubscribe(SlashCommandInteractionEvent event) {
         event.reply("Not implemented yet").queue();
     }
 
     @ICommand(name = "list", description = "List all events from a Google Calendar")
-    @IArgument(name = "Name", description = "The name to use for the calendar", required = false)
+    @IArgument(name = "name", description = "The name to use for the calendar", required = false)
     public static void list(SlashCommandInteractionEvent event) {
         event.reply("Not implemented yet").queue();
     }
 
-    @ICommand(name = "Update", description = "Update configuration of a calendar", permissions = {Permission.ADMINISTRATOR})
-    @IArgument(name = "Name", description = "The name to use for the calendar", required = true)
-    @IArgument(name = "Channel", description = "The channel where events will be announced", required = true, type = OptionType.CHANNEL)
-    @IArgument(name = "Role", description = "The role to be pinged when events begin", required = false, type = OptionType.ROLE)
+    @ICommand(name = "update", description = "Update configuration of a calendar", permissions = {Permission.ADMINISTRATOR})
+    @IArgument(name = "name", description = "The name to use for the calendar", required = true)
+    @IArgument(name = "channel", description = "The channel where events will be announced", required = true, type = OptionType.CHANNEL)
+    @IArgument(name = "role", description = "The role to be pinged when events begin", required = false, type = OptionType.ROLE)
     public static void update(SlashCommandInteractionEvent event) {
         event.reply("Not implemented yet").queue();
     }
